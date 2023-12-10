90afcd5fef9ad59dd58ac5de7e8bf09a88e7b6e4
navid dadkhah <nnd1380@gmail.com>
2023-12-10 16:16:30 +0330

    adding login page

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import './Auth.css'
+import Logo from './Images/logo.jpg'
 
 export const Auth = () => {
-  return (
-    <div className='login-page'>
-          <div className="image-side">image</div>
-          <div className="auth-side">
-            <input type="text" />
-          </div>
-    </div>
-  )
+    return (
+        <div className="background-image-login">
+            <div className="auth-side">
+               <div className="cards">
+                 <div className="logo"><img src={Logo} alt="Logo" /></div>
+                    <div className="title">سامانه هوشمند متخصص یاب ایرانی (ساهمتا)</div>
+                 <div className="inputs">
+                    <form >
+                        <label className='labels' htmlFor="username">نام کاربری</label>
+                            <input
+                                type="text"
+                                className="infoInput"
+                                name="username"
+                                placeholder="نام کاربری"
+
+                            />
+                            <label className='labels' htmlFor="password">رمز عبور</label>
+                            <input
+                                type="email"
+                                className="infoInput"
+                                name="password"
+                                placeholder="رمز عبور"
+
+                            />
+                            
+                    </form>
+                       
+                 </div>
+               </div>
+            </div>
+        </div>
+    )
 }
 
