3d2dbd70f22dd0d7ef7a8e619d91a8955c50a9af
navid dadkhah <nnd1380@gmail.com>
2023-12-10 17:52:18 +0330

    fix the login page

---
+++ src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Button.css'
+export const Button = () => {
+  return (
+    <div className='button'>
+          <div className='submit'> 
+             ثبت
+          </div>
+    </div>
+  )
+}
+
--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,17 +1,64 @@
 import React from 'react'
 import './Auth.css'
 import Logo from './Images/logo.jpg'
+import 'react-toastify/dist/ReactToastify.css';
+import  '../Components/Button'
+import { ToastContainer, toast } from 'react-toastify';
 
 export const Auth = () => {
+
+    const handleSubmit= (e)=>{
+        e.preventDefault()
+        notify(".کاربر وارد شد", "success")
+    }
+
+const notify = (msg, type) => {
+    if (type === "error") {
+        toast.error(msg, {
+            position: "top-left",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined,
+            theme: "light",
+        });
+    }
+    else if (type === "success") {
+        toast.success(msg, {
+            position: "top-left",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined,
+            theme: "light",
+            className:"notification"
+        });
+    }
+}
     return (
         <div className="background-image-login">
-            <div className="auth-side">
-               <div className="cards">
-                 <div className="logo"><img src={Logo} alt="Logo" /></div>
-                    <div className="title">سامانه هوشمند متخصص یاب ایرانی (ساهمتا)</div>
-                 <div className="inputs">
+          <div className="auth-side">
+            <div className="cards">
+                <div className="logo">
+                    <img src={Logo} alt="Logo" />
+                 </div>
+                <div className="title">
+                        <span>
+                          سامانه هوشمند متخصص یاب ایرانی 
+                        </ span>
+                        <span>
+                          (ساهمتا)
+                        </span>
+                 </div>
+                <div className="inputs">
                     <form >
-                        <label className='labels' htmlFor="username">نام کاربری</label>
+                        <div className='labels'>
+                           <label htmlFor="username">: نام کاربری</label>
+                        </div> 
                             <input
                                 type="text"
                                 className="infoInput"
@@ -19,20 +66,29 @@ export const Auth = () => {
                                 placeholder="نام کاربری"
 
                             />
-                            <label className='labels' htmlFor="password">رمز عبور</label>
+                        <div className='labels labels2'>
+                            <label htmlFor="password">: رمز عبور</label>
+                        </div> 
                             <input
                                 type="email"
                                 className="infoInput"
                                 name="password"
                                 placeholder="رمز عبور"
-
                             />
-                            
+                            <button
+                                className="button"
+                                id="infoButton"
+                                type="submit"
+                                onClick={handleSubmit}
+                            >
+                                ثبت
+                            </button>
+                            <ToastContainer />
                     </form>
                        
                  </div>
-               </div>
             </div>
+          </div>
         </div>
     )
 }
