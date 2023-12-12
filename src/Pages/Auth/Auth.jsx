943c6b2cd674b1ed213a2e3bdb4c8cffa8ca31cc
navid dadkhah <nnd1380@gmail.com>
2023-12-12 14:38:04 +0330

    fix login page

--- src/Pages/Auth/Auth.jsx
+++ src/Pages/Auth/Auth.jsx
@@ -48,58 +48,62 @@ const notify = (msg, type) => {
     }
 }
     return (
-        <div className="background-image-login">
-          <div className="auth-side">
-            <div className="cards">
-                <div className="logo">
-                    <img src={Logo} alt="Logo" />
-                 </div>
-                <div className="title">
-                        <span>
-                          سامانه هوشمند متخصص یاب ایرانی 
-                        </ span>
-                        <span>
-                          (ساهمتا)
-                        </span>
-                 </div>
-                <div className="inputs">
-                    <form >
-                        <div className='labels'>
-                           <label htmlFor="username">: نام کاربری</label>
-                        </div> 
-                            <input
-                                type="text"
-                                className="infoInput"
-                                name="username"
-                                placeholder="نام کاربری"
-                            />
-                        <div className='labels labels2'>
-                            <label htmlFor="password">: رمز عبور</label>
-                        </div> 
-                            <div className="password-input-container">
-                            <input
-                                type={showPassword? "text" : "password"}
-                                className="infoInput"
-                                name="password"
-                                placeholder="رمز عبور"
-                            />
-                            <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
-                                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-                            </span>
-                            </div>
-                            <button
-                                className="button"
-                                id="infoButton"
-                                type="submit"
-                                onClick={handleSubmit}
-                            >
-                                ثبت
-                            </button>
-                            <ToastContainer limit={5} rtl className={"notification"} />
-                    </form>
-                 </div>
-            </div>
-          </div>
+        <div className="background-login">
+            <div className="backgroung-login-left"></div>
+
+            <div className='background-login-right'>
+                <div className="auth-side">
+                    <div className="cards">
+                        <div className="logo">
+                            <img src={Logo} alt="Logo" />
+                        </div>
+                        <div className="title">
+                                <span>
+                                سامانه هوشمند متخصص یاب ایرانی 
+                                </ span>
+                                <span>
+                                (ساهمتا)
+                                </span>
+                        </div>
+                        <div className="inputs">
+                            <form >
+                                <div className='labels'>
+                                <label htmlFor="username">: نام کاربری</label>
+                                </div> 
+                                    <input
+                                        type="text"
+                                        className="infoInput"
+                                        name="username"
+                                        placeholder="نام کاربری"
+                                    />
+                                <div className='labels labels2'>
+                                    <label htmlFor="password">: رمز عبور</label>
+                                </div> 
+                                    <div className="password-input-container">
+                                    <input
+                                        type={showPassword? "text" : "password"}
+                                        className="infoInput"
+                                        name="password"
+                                        placeholder="رمز عبور"
+                                    />
+                                    <span className="password-toggle-icon" onClick={togglePasswordVisibility}>
+                                        <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                                    </span>
+                                    </div>
+                                    <button
+                                        className="button"
+                                        id="infoButton"
+                                        type="submit"
+                                        onClick={handleSubmit}
+                                    >
+                                        ثبت
+                                    </button>
+                                    <ToastContainer limit={5} rtl className={"notification"} />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+             </div>    
         </div>
     )
 }
