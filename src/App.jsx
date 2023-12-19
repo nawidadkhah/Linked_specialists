f2962aebdd4b4a07318c4ba5f3f0c4ef387343ff
navid dadkhah <nnd1380@gmail.com>
2023-12-19 16:53:46 +0330

    add search button in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Auth } from './Pages/Auth/Auth'
 import { Home } from './Pages/Home/Home'
+import { SearchPage } from "./Pages/SearchPage/SearchPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 
@@ -10,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Auth />}></Route>
           <Route path="/home" element={<Home />}></Route>
-
+          <Route path="/Search" element={<SearchPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,39 @@
-import React from 'react'
+import React from "react";
 import Logo from "../../Images/logo.jpg";
-import './Navbar.css'
+import { SearchPage } from "../../Pages/SearchPage/SearchPage.jsx";
+import { Link } from "react-router-dom";
+
+import "./Navbar.css";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="nav-img">
-        <img src={Logo} alt="Logo" />
+        <Link to="/home">
+          <img src={Logo} alt="Logo" />
+        </Link>
         <span>سامانه هوشمند متخصص یاب ایرانی</span>
       </div>
       <ul className="nav-tools">
-        
+        <Link to="/Search">
           <a href="#" className="items">
             گزارشات
           </a>
-          <a href="#" className="items">
-            کلید واژه‌های تخصصی
-          </a>
+        </Link>
+        <a href="#" className="items">
+          کلید واژه‌های تخصصی
+        </a>
 
-          <a href="#" className="items">
-            تنظیمات مدیریت کاربران
-          </a>
-          <a href="#" className="items">
-            روند علمی متخصصین
-          </a>
-          <a href="#" className="items">
-            جستجوی متخصص
-          </a>
+        <a href="#" className="items">
+          تنظیمات مدیریت کاربران
+        </a>
+        <a href="#" className="items">
+          روند علمی متخصصین
+        </a>
+        <a href="#" className="items">
+          جستجوی متخصص
+        </a>
       </ul>
     </div>
   );
-}
+};
