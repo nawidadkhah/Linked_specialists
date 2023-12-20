298aa3482d2347c14f50347276b459955d5049c9
navid dadkhah <nnd1380@gmail.com>
2023-12-20 15:07:58 +0330

    fix modal

--- src/Components/PeopleCard/PeopleCard.jsx
+++ src/Components/PeopleCard/PeopleCard.jsx
@@ -1,21 +1,47 @@
 import React from "react";
+import ax from '../../Images/about.jpg'
 import "./PeopleCard.css";
+import '../../Pages/SearchPage/SearchPage.css'
 
 export const PeopleCard = (props) => {
+
+    const handleClick =()=>{
+
+        document.querySelectorAll(".modal").forEach(function (el) {
+          el.style.display = "flex";
+        });
+         document.body.style.overflow = "hidden";
+
+       props.setData({
+         university: "",
+         name: props.name,
+         image: ax,
+         job: "استاد دانشگاه",
+         city: "آلبرتا، کانادا",
+         email: "nnd1380@gmail",
+         phone: "09109207102",
+         site: "www.ccc.com",
+       });
+    }
+
   return (
     <div className="peopleCard">
-      <div className="peopleCard-image">ax</div>
+      <div className="peopleCard-image">
+        <img src={ax} alt="" />
+      </div>
       <div className="peopleCard-name">{props.name}</div>
       <div className="peopleCard-items">
         <ul className="peopleCard-list">
           <i className="people-list-item">مهارت در حوزه:</i>
           <i className="people-list-item">هوش مصنوعی</i>
-          <i className="people-list-item">ابیای</i>
-          <i className="people-list-item">یببب</i>
+          <i className="people-list-item">بینایی کامپیوتر</i>
+          <i className="people-list-item">یادگیری ماشین</i>
         </ul>
       </div>
       <div className="peopleCard-button">
-        <button>ادامه</button>
+        {/* <Link to="/"> */}
+            <button onClick={handleClick}>دیدن پروفایل</button>
+        {/* </Link> */}
       </div>
     </div>
   );
--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,35 +1,62 @@
 import React, { useState } from "react";
 import { Navbar } from "../../Components/Navbar/Navbar";
 import { PeopleCard } from "../../Components/PeopleCard/PeopleCard";
+import { Modal } from "../../Components/Modal/Modal";
 import "./SearchPage.css";
 
 export const SearchPage = () => {
   const [isSearch, SetisSearch] = useState(false);
+  const [data, SetData] = useState({
+    university: "",
+    name:"aliiii",
+    image: "",
+    job: "",
+    city: "abs",
+    email: "",
+    phone: "",
+    site: "",
+  });
+
+  const handleSearch= ()=>{
+SetisSearch(true)
+  }
   return (
     <div>
-      <Navbar />
-      <div className="search-div">
-        <div className="search-div-items">
-          <label for="specialWork">حوزه تخصصی:</label>
-          <select id="specialWork" name="specialWork">
-            <option value="default"></option>
-            <option value="ai">هوش مصنوعی</option>
-          </select>
+      <Modal data={data} />
+      <div className="search-content">
+        <Navbar />
+        <div className="search-div">
+          <div className="search-div-items">
+            <label for="specialWork">حوزه تخصصی:</label>
+            <select id="specialWork" name="specialWork">
+              <option value="default"></option>
+              <option value="ai">هوش مصنوعی</option>
+            </select>
+          </div>
+          <div className="search-div-items">
+            <label for="specialWord">کلید واژه تخصصی:</label>
+            <select id="specialWord" name="specialWord">
+              <option value="default"></option>
+              <option value="ai">----------------------</option>
+            </select>
+          </div>
+          <button className="search-page-button" onClick={handleSearch}>
+            جستجو
+          </button>
         </div>
-        <div className="search-div-items">
-          <label for="specialWord">کلید واژه تخصصی:</label>
-          <select id="specialWord" name="specialWord">
-            <option value="default"></option>
-            <option value="ai">----------------------</option>
-          </select>
-        </div>
-        <button className="search-page-button">جستجو</button>
-      </div>
-      <div className="search-items">
-        <PeopleCard name="ali"/>
-        <PeopleCard />
-        <PeopleCard />
-        <PeopleCard />
+        {isSearch && (
+          <div className="search-items">
+            <PeopleCard name="علی مردانی" setData={SetData} />
+            <PeopleCard name="علی مردانی" setData={SetData} />
+            <PeopleCard name="علی مردانی" setData={SetData} />
+            <PeopleCard name="علی مردانی" setData={SetData} />
+            <PeopleCard name="علی مردانی" setData={SetData} />
+            <PeopleCard name="علی مردانی" setData={SetData} />
+            <PeopleCard name="علی مردانی" setData={SetData} />
+            <PeopleCard name="علی مردانی" setData={SetData} />
+   
+          </div>
+        )}
       </div>
     </div>
   );
