84c76094f29856957c28ae7c86851718a5499455
navid dadkhah <nnd1380@gmail.com>
2023-12-20 23:29:55 +0330

    adding special word page

---
+++ src/Pages/SpecialWord/SpecialWord.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { SearchCom } from '../../Components/SearchCom/SearchCom'
+import { Navbar } from '../../Components/Navbar/Navbar'
+import { Table } from '../../Components/Table/Table'
+import "./SpecialWord.css"
+
+export const SpecialWord = () => {
+  const [isSearch, setIsSearch] = useState(false);
+
+  return (
+    <div>
+      <Navbar />
+      <SearchCom
+        title="حوزه تخصصی"
+        option="هوش مصنوعی"
+        setIsSearch={setIsSearch}
+      />
+      {isSearch && (
+        <div>
+          <Table
+            firstTh="رصد تکرار در حوزه تخصصی"
+            secTh="کلیدواژه"
+            thirdTh="جایگاه"
+            firstTd1="Machine Learning"
+            firstTd2="90%"
+            firstTd3="1"
+            secTd1="Image Processing"
+            secTd2="40%"
+            secTd3="2"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
